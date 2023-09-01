86f5ecb0db1460785643b8af6fdd773fde54012e
Jacob Smith <jsmithdenverdev@gmail.com>
2023-08-31 23:31:41 -0600

    limited re-renders as state changes

--- shared-context/packages/host/src/components/RemoteModule.tsx
+++ shared-context/packages/host/src/components/RemoteModule.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { resolveRemote } from "../utils/resolve-remote";
 
+function useLazy(name: string, importCall: any) {
+  const lazyComponentsStore = React.useRef<{ [name: string]: any }>({}).current;
+  if (!lazyComponentsStore[name]) {
+    lazyComponentsStore[name] = React.lazy(importCall);
+  }
+  return lazyComponentsStore[name];
+}
+
 const RemoteModule = ({ scope, module }: { scope: string; module: string }) => {
-  const Remote = React.lazy(() => resolveRemote(scope, module));
+  const Remote = useLazy(`${scope}-${module}`, () =>
+    resolveRemote(scope, module)
+  );
 
   return (
     <React.Suspense fallback={<div>Loading...</div>}>
